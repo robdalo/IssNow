f8520eacdd1033fddf128b590bba5b659be40d60
Rob Daley <robd@itaboi.co.uk>
2020-08-12 23:39:55 +0100

    Revised iss-now component to auto refresh the location data.

--- IssNow.Web/ClientApp/src/app/iss-now/iss-now.component.ts
+++ IssNow.Web/ClientApp/src/app/iss-now/iss-now.component.ts
@@ -7,11 +7,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class IssNowComponent {
   public location: Location;
+  public interval: any;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.refreshData(http, baseUrl);
+    this.interval = setInterval(() => {
+      this.refreshData(http, baseUrl);
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.interval)
+      clearInterval(this.interval);
+  }
+
+  refreshData(http: HttpClient, baseUrl: string) {
     http.get<Location>(baseUrl + 'location').subscribe(result => {
       this.location = result;
-    }, error => console.error(error));
+    }, error => console.error(error))
   }
 }
 
